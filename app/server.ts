2e9c8538f4c2f9b9ba8fd9fbc41e85cb1bb374ad
Matsui Masashi <5251092+circled9@users.noreply.github.com>
2024-02-11 11:35:01 +0900

    serve static assets

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,15 @@
+import { serveStatic } from '@hono/node-server/serve-static'
+import { Hono } from 'hono'
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
-const app = createApp()
+const hono: Hono = new Hono()
+hono.get('/assets/*', serveStatic({ root: './public' }))
+
+const app = createApp({
+  // @ts-ignore
+  app: hono,
+})
 
 showRoutes(app)
 
